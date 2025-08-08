5e51dd813ec92bed753d0da0472940fd58089a7d
OmoPro-cloud <omopro@yahoo.com>
2025-08-08 15:07:11 -0500

    Create oop_assignment_1.py

---
+++ oop_assignment_1.py
@@ -0,0 +1,39 @@
+Assignment – Extended Dog Class
+Modify the Dog class to include the following:
+
+An additional attribute called breed.
+
+A method called celebrate_birthday() that increases the dog's age by 1 and returns a message like:
+"Buddy is now 6 years old. Happy Birthday!"
+
+Update the walk() method so it returns a message like:
+"Buddy is walking happily in the park."
+
+Add a method rename(new_name) to change the dog's name.
+
+Create at least 3 dog objects with different names, ages, and breeds.
+Demonstrate usage of all methods in your program:
+
+Make each dog bark.
+
+Print their age and breed.
+
+Make one dog celebrate its birthday.
+
+Rename one dog.
+
+Make all dogs walk.
+
+Bonus Task (Optional):
+Create a method compare_age(other_dog) that compares two dogs' ages and returns a message stating which dog is older.
+
+
+Expected Skills Practiced:
+
+__init__ constructor
+
+Instance variables
+
+Writing multiple methods
+
+Passing objects as parameters (bonus)
