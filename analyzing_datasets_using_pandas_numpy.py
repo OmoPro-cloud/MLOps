b9bda0ebde4445998f5cefa670cf2aaf31d2eb00
OmoPro-cloud <omopro@yahoo.com>
2025-08-16 22:28:00 -0500

    Update analyzing_datasets_using_pandas_numpy.py

--- analyzing_datasets_using_pandas_numpy.py
+++ analyzing_datasets_using_pandas_numpy.py
@@ -6,7 +6,7 @@ Load with Pandas.
 
 Use filtering to show top 10 countries by total cases.
 
-Use groupby to see cases per continent.
+Use groupby to see deaths per continent.
 
 Use NumPy to calculate percentage changes over time.
 '''
@@ -23,7 +23,17 @@ top10_cases = latest_data.sort_values('total_cases', ascending=False).head(10)
 print('Top 10 countries by local cases:')
 print(top10_cases)
 
-#use groupby to see cases per continent
+#use groupby to see deaths per continent
 total_death_by_country = covid_df.groupby('country')['total_deaths'].max().reset_index()
 print('\nTotal deaths by country:')
 print(total_death_by_country)
+
+#use numpy to see percentage changes over time
+country_data = covid_df[covid_df['country'] == 'Asia'].sort_values('date')
+country_data['new_cases'] = country_data['Cases'].diff().fillna()
+country_data['percentage_change'] = np.where(country_data['new_cases'] > 0,
+                                             (country_data['new_cases'] / country_data['Cases'].shift(1)) * 100, 0)
+print('\nPercentage changes of new cases in Asia:')
+print(country_data[['date', 'new_cases', 'percentage_change']].head(15))
+
+#pick ebola dataset and do the same thing we have done with covid 19
