83cd9c8186611161d4f5460926ff3a13d3536eab
OmoPro-cloud <omopro@yahoo.com>
2025-08-23 14:35:39 -0500

    Update learning_seaborn.py

--- learning_seaborn.py
+++ learning_seaborn.py
@@ -8,5 +8,9 @@ df = pd.read_csv('time_series_demo.csv')
 sns.set_theme(style='whitegrid', content='talk')
 
 #core plot
+df_temp = df.rename(columns={'Value': 'temp_anomaly', 'Region':'region'})
+df_temp['date'] = pd.to_datetime(df_temp['Year'], format='%Y')
+df_temp['year'] = df_temp['date'].dt.year
+df_temp['month'] = df_temp['date'].dt.month
 #df_temp = 
-#sns.lineplot(data=)
+sns.lineplot(data=df_temp, x='Year', y='Value')
