8aef46322acd62df53786a1a3d67c2bd0bfdeb70
OmoPro-cloud <omopro@yahoo.com>
2025-08-23 16:01:14 -0500

    Update learning_seaborn.py

--- learning_seaborn.py
+++ learning_seaborn.py
@@ -1,6 +1,7 @@
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.express as px
 
 #load data
 
@@ -22,6 +23,22 @@ plt.title('Global temperature anomaly ')
 plt.show()
 
 #distribution
-sns.histplot(data=df_temp, x='temp_anomaly', bins=10, kde=True)
+sns.histplot(data=df_temp, x='temp_anomaly', bins=10, kde=True)#histplot is used for distribution
 plt.title('Distribution of gloabal temperature anomalies')
 plt.show()
+'''
+To Do: a categorical comparison(.boxplot) and heatmap
+'''
+
+#Faceting for small multiples
+g = sns.relplot(data=df_temp, x='year', y='temp_anomaly', col='region', kind='line', col_wrap=3, height=2.6)
+g.figure.suptitle('Regional Trends', y=1.02)
+plt.show()
+
+#plotly is interactive
+#fig = px.line(df_temp, x='year', y='temp_anomaly', color='region', title='Interactive: Gloabl Temperature Anomalies by Region')
+fig = px.histogram(df_temp, x='temp_anomaly', nbins=30, marginal='box', title='Distribution of global temperatue anomalies')
+fig.update_layout(legend_title_text='Region')
+fig.write_html('temp_anomalies.html')
+fig.show()
+
