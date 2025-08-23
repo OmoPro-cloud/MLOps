e1c3eae8d902b27b180a34e180070160e1db342b
OmoPro-cloud <omopro@yahoo.com>
2025-08-23 14:23:43 -0500

    first time using seaborn

---
+++ learning_seaborn.py
@@ -0,0 +1,12 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#load data
+
+df = pd.read_csv('time_series_demo.csv')
+sns.set_theme(style='whitegrid', content='talk')
+
+#core plot
+#df_temp = 
+#sns.lineplot(data=)
