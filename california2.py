52fb3727bf9debed6cb96557a283fc15fb8e2a41
OmoPro-cloud <omopro@yahoo.com>
2025-09-20 21:12:31 -0500

    Update california2.py

--- california2.py
+++ california2.py
@@ -1,6 +1,6 @@
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
 import numpy as np
-from sklearn.metrics import r2_score
+from sklearn.metrics import classification_report, r2_score, roc_auc_score
 from sklearn.compose import ColumnTransformer
 from sklearn.datasets import fetch_california_housing
 import pandas as pd
@@ -19,7 +19,7 @@ df = data.frame
 X = df.drop(columns=['MedHouseVal'])
 y = df['MedHouseVal']
 
-#Train-test split
+#Train-test split(train linear regression)
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 #Pre-processing (numeric only here)
@@ -48,15 +48,36 @@ mae = mean_absolute_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 print(f"RMSE: {rmse:.3f}, MAE: {mae:.3f}, R2: {r2:.3f}")
 
-# Baseline: always predict mean of training y
+# Baseline
 baseline_pred = np.full(shape=y_test.shape, fill_value=np.mean(y_train))
 baseline_mse = mean_squared_error(y_test, baseline_pred)
 baseline_rmse = np.sqrt(baseline_mse)
 print(f"Baseline RMSE: {baseline_rmse:.3f}")
 
 '''
-RMSE:
+RMSE: Root Means Squared Error
 MAE: Mean Absolute Error - shows that our predictions could be off by 53.3%
-R2: Route Squared - 
+R2: Route Squared - this metric is used to judge how well a regression model fits the data
 Standard Scaler will standardize everything, it will put all features on a similar scale(yen, naira, euros converted to dollars) before comparing them
 '''
+
+#binary target
+df = data.frame.copy()
+df['High_Price'] = (df['MedHouseVal'] > df['MedHouseVal'].median()).astype(int)
+X = df.drop(columns=['MedHouseVal', 'High_Price'])
+y = df['High_Price']
+
+x_tr, x_te, y_tr, y_te = train_test_split(X, y, test_size=0.2, random_state=42)
+#print(df.head())
+
+#Train logistics regression
+pipe_clf = Pipeline([
+  ('pre', preprocessor),
+  ('model', LogisticRegression())
+])
+pipe_clf.fit(x_tr, y_tr)
+y_hat = pipe_clf.predict(x_te)
+
+#Metrics
+print(classification_report(y_te, y_hat))
+print("ROC AUC:", roc_auc_score(y_te, pipe_clf.predict_proba(x_te)[:, 1]))
