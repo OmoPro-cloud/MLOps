5e4d790dc6ff841e54267c4ad614cbe91965c4c9
OmoPro-cloud <omopro@yahoo.com>
2025-10-01 04:36:14 -0500

    Update k_means_clustering_assignment.py
    Plotting graphs

--- k_means_clustering_assignment.py
+++ k_means_clustering_assignment.py
@@ -6,7 +6,7 @@ import numpy as np
 #Generate synthetic data
 X, _ = make_blobs(n_samples=400, centers=3, random_state=42)
 
-#K-Means Implementation
+# K-Means Implementation
 
 #Model 1
 kmeans_optimal = KMeans(n_clusters=3, random_state=0)
@@ -18,5 +18,23 @@ kmeans_suboptimal = KMeans(n_clusters=5, random_state=0)
 labels_suboptimal = kmeans_suboptimal.fit_predict(X)
 centroids_suboptimal = kmeans_suboptimal.cluster_centers_
 
-#Task 3: Visualization
+# Visualization
 fig, axes = plt.subplots(1, 2, figsize=(14, 6))
+
+#Plotting graphs
+#plot 1
+axes[0].scatter(X[:, 0], X[:, 1], c=labels_optimal, cmap='viridis', s=30)
+axes[0].scatter(centroids_optimal[:, 0], centroids_optimal[:, 1], 
+                marker='X', s=200, c='red', label='Centroids')
+axes[0].set_title("Optimal Clustering: k=3")
+axes[0].legend()
+
+#plot 2
+axes[1].scatter(X[:, 0], X[:, 1], c=labels_suboptimal, cmap='viridis', s=30)
+axes[1].scatter(centroids_suboptimal[:, 0], centroids_suboptimal[:, 1], 
+                marker='X', s=200, c='red', label='Centroids')
+axes[1].set_title("Suboptimal Clustering: k=5")
+axes[1].legend()
+
+plt.tight_layout()
+plt.show()
