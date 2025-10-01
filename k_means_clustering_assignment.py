f1bae076309ac43b1b58841dca273a2ecbfdc964
OmoPro-cloud <omopro@yahoo.com>
2025-10-01 04:28:00 -0500

    Update k_means_clustering_assignment.py
    creating synthetic data and creating the first model, setting k to 3(this is meant to be more optimal)

--- k_means_clustering_assignment.py
+++ k_means_clustering_assignment.py
@@ -2,3 +2,14 @@ from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 import numpy as np
+
+# Generate synthetic data
+X, _ = make_blobs(n_samples=400, centers=3, random_state=42)
+
+#K-Means Implementation
+
+# Model 1
+kmeans_optimal = KMeans(n_clusters=3, random_state=0)
+labels_optimal = kmeans_optimal.fit_predict(X)
+centroids_optimal = kmeans_optimal.cluster_centers_
+
