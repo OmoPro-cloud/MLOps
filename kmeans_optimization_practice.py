d3f294e1c490ec7842df5e0258a6adf6d8838010
OmoPro-cloud <omopro@yahoo.com>
2025-10-03 04:46:13 -0500

    Create kmeans_optimization_practice.py

---
+++ kmeans_optimization_practice.py
@@ -0,0 +1,11 @@
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans #KMeans is a clustering algorithm from sci-kit learn
+import numpy as np
+
+#Generate synthetic data
+X, y = make_blobs(n_samples=400, centers=3, random_state=42)
+
+kmeans_optimal = KMeans(n_clusters=3, random_state=0)
+labels_optimal = kmeans_optimal.fit_predict(X)
+
