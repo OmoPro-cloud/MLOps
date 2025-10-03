f3c8f32f132858067ab00bc46da37715f197de09
OmoPro-cloud <omopro@yahoo.com>
2025-10-03 04:46:17 -0500

    Update k_means_clustering_assignment.py

--- k_means_clustering_assignment.py
+++ k_means_clustering_assignment.py
@@ -9,12 +9,12 @@ X, _ = make_blobs(n_samples=400, centers=3, random_state=42)
 # K-Means Implementation
 
 #Model 1
-kmeans_optimal = KMeans(n_clusters=3, random_state=0)
+kmeans_optimal = KMeans(n_clusters=3, random_state=0) #shows what optimal clustering looks like
 labels_optimal = kmeans_optimal.fit_predict(X)
 centroids_optimal = kmeans_optimal.cluster_centers_
 
 #Model 2
-kmeans_suboptimal = KMeans(n_clusters=5, random_state=0)
+kmeans_suboptimal = KMeans(n_clusters=5, random_state=0) #this gives us an example of suboptimal clustering
 labels_suboptimal = kmeans_suboptimal.fit_predict(X)
 centroids_suboptimal = kmeans_suboptimal.cluster_centers_
 
@@ -25,14 +25,14 @@ fig, axes = plt.subplots(1, 2, figsize=(14, 6))
 #plot 1
 axes[0].scatter(X[:, 0], X[:, 1], c=labels_optimal, cmap='viridis', s=30)
 axes[0].scatter(centroids_optimal[:, 0], centroids_optimal[:, 1], 
-                marker='X', s=200, c='red', label='Centroids')
+                marker='o', s=100, c='red', label='Center')
 axes[0].set_title("Optimal Clustering: k=3")
 axes[0].legend()
 
 #plot 2
 axes[1].scatter(X[:, 0], X[:, 1], c=labels_suboptimal, cmap='viridis', s=30)
 axes[1].scatter(centroids_suboptimal[:, 0], centroids_suboptimal[:, 1], 
-                marker='X', s=200, c='red', label='Centroids')
+                marker='o', s=100, c='red', label='Center')
 axes[1].set_title("Suboptimal Clustering: k=5")
 axes[1].legend()
 
