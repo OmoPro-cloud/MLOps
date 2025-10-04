01bd6e82d5c0e060e5e2d195d332f956dcd89d15
OmoPro-cloud <omopro@yahoo.com>
2025-10-04 03:37:15 -0500

    Update vizualizing_data_with_pca.py

--- vizualizing_data_with_pca.py
+++ vizualizing_data_with_pca.py
@@ -10,3 +10,14 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import matplotlib.pyplot as plt
+
+#load data
+data = fetch_california_housing(as_frame=True)
+df = data.frame
+
+#create feature and target
+X = df.drop(columns=['MedHouseVal'])
+y = df['MedHouseVal']
+
+#train test split
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
