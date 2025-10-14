1699d34f48d52f23431046c3f79f4f7aa20bf92a
OmoPro-cloud <omopro@yahoo.com>
2025-10-14 02:21:58 -0500

    .

---
+++ mlp_practice_with_different_layer_sizes.py
@@ -0,0 +1,53 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers 
+import matplotlib.pyplot as plt
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+plt.figure(figsize=(6, 6))
+for i in range(9):
+  plt.subplot(3, 3, i + 1)
+  plt.imshow(x_train[i], cmap='gray')
+  plt.title(f"Label: {y_train[i]}")
+  plt.axis('off')
+  plt.show()
+
+firstModel = keras.Sequential([
+  layers.Flatten(input_shape=(28, 28)),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(64, activation='relu'),
+  layers.Dense(10, activation='softmax')
+])
+
+secondModel = keras.Sequential([
+  layers.Flatten(input_shape=(28, 28)),
+  layers.Dense(64, activation='relu'),
+  layers.Dense(32, activation='relu'),
+  layers.Dense(10, activation='softmax')
+])
+
+thirdModel = keras.Sequential([
+  layers.Flatten(input_shape=(28, 28)),
+  layers.Dense(256, activation='relu'),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(10, activation='softmax')
+])
+
+firstModel.compile(optimizer='adam',
+                   loss='sparse_categorical_crossentropy',
+                   metrics=['accuracy'])
+firstHistory = firstModel.fit(x_train, y_train, epochs=5, batch_size=32, validation_split=0.2)
+
+secondModel.compile(optimizer='adam',
+                    loss='sparse_categorical_crossentropy',
+                    metrics=['accuracy'])
+secondHistory = secondModel.fit(x_train, y_train, epochs=5, batch_size=32, validation_split=0.2)
+
+thirdModel.compile(optimizer='adam',
+                   loss='sparse_categorical_crossentropy',
+                   metrics=['accuracy'])
+
