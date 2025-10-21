0720c091e7ceac2122da50ebe4515ae2ddccb674
OmoPro-cloud <omopro@yahoo.com>
2025-10-21 02:35:48 -0500

    Update cnn_practice.py

--- cnn_practice.py
+++ cnn_practice.py
@@ -6,3 +6,12 @@ from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 
 (x_train, y_train), (x_test, y_test) = keras.datasets.fashion_mnist.load_data()
+
+#normalize range from 0 - 1
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+#reshape the values to include a grayscale channel(allows color)
+x_train = x_train.reshape(1, 28, 28, -1)
+x_test = x_test.reshape(1, 28, 28, -1)
+
