d81d7e2df0914e3f87f85538235728c36c470d24
OmoPro-cloud <omopro@yahoo.com>
2025-11-08 18:41:29 -0600

    Update fine_tuning_and_transfer_learning_with_mobilenet_and_resnet.py

--- fine_tuning_and_transfer_learning_with_mobilenet_and_resnet.py
+++ fine_tuning_and_transfer_learning_with_mobilenet_and_resnet.py
@@ -1,14 +1,10 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing import ImageDataGenerator
 import matplotlib.pyplot as plt
 import numpy as np
 
 train_datagen = ImageDataGenerator(
   rescale=1./255,
-  rotation_range=20,
-  zoom_range=0.2,
-  horizontal_flip=True
+  
 )
-
-val_data = ImageDataGenerator(rescale=1./255)
